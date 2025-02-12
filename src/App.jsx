5a1e90270ea93bb57548afee17cce5e6324e2d62
Mohana-j <mohanapriyajayakumar25@gmail.com>
2025-02-12 19:36:20 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
-import Dashboard from "./components/dashboard";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
